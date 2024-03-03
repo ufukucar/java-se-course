419312a7e12a572b42736820ded6d487241052cd
ufukucar <ufuk.ucar@tuseb.gov.tr>
2024-03-03 23:19:46 +0300

    methods

---
+++ java-006-methods/src/main/java/com/ufukucar/Method_01.java
@@ -0,0 +1,25 @@
+package com.ufukucar;
+
+public class Method_01 {
+
+
+    public static void main(String[] args) {
+
+
+        cizgiCiz();
+        cizgiCiz();
+        cizgiCiz();
+        cizgiCiz();
+        cizgiCiz();
+        cizgiCiz();
+
+    }
+
+    private static void cizgiCiz() {
+
+        System.out.println("----------------------------------");
+
+    }
+
+
+}
