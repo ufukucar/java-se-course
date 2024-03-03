4edf5a93c5894f9a781be63c7449bd1107dfdc37
ufukucar <ufuk.ucar@tuseb.gov.tr>
2024-03-03 18:00:13 +0300

    Variables değişkenler

--- java-001-hello-world/src/main/java/org/example/Main.java
+++ java-001-hello-world/src/main/java/org/example/Main.java
@@ -6,5 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        int i = 0;
+
+        ++i;
+        i++;
+        System.out.println(i);
     }
 }
---
+++ java-002-variable/src/main/java/org/ufukucar/Main2.java
@@ -0,0 +1,4 @@
+package org.ufukucar;
+
+public class Main2 {
+}
