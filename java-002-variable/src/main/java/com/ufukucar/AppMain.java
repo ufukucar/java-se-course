dcd7e63268d7099a9b8cae91b9d292023d57aa7b
ufukucar <ufuk.ucar@tuseb.gov.tr>
2024-03-03 19:38:52 +0300

    Variables değişkenler

--- java-002-variable/src/main/java/com/ufukucar/AppMain.java
+++ java-002-variable/src/main/java/com/ufukucar/AppMain.java
@@ -8,6 +8,18 @@ public class AppMain {
         System.out.println("App MAIN");
 
 
+        int sayi1 = 15;
+        int sayi2 = 35;
+        int sonuc = sayi1 + sayi2;
 
+
+        System.out.println("sayi1: " + sayi1);
+        System.out.println("sayi2: " + sayi2);
+        System.out.println("Sonuc: " + sonuc);
+
+        System.out.println("Toplama: " + (sayi1 + sayi2));
+        System.out.println("Cikarma: " + (sayi2 - sayi1));
+        System.out.println("Bölme: " + (sayi2 / sayi1));
+        System.out.println("Çarpma: " + (sayi1 * sayi2));
     }
 }
---
+++ java-003-loops/src/main/java/com/ufukucar/AppMain.java
@@ -0,0 +1,10 @@
+package com.ufukucar;
+
+
+public class AppMain {
+    public static void main(String[] args) {
+
+
+
+    }
+}
