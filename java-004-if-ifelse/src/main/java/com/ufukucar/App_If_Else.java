3966c75805d5948dd91e1b70591ea8d17c085749
ufukucar <ufuk.ucar@tuseb.gov.tr>
2024-03-03 23:18:57 +0300

    if - else

---
+++ java-004-if-ifelse/src/main/java/com/ufukucar/App_If_Else.java
@@ -0,0 +1,28 @@
+package com.ufukucar;
+
+public class App_If_Else {
+    public static void main(String[] args) {
+
+        int sayi = 3;
+
+        if ( 5 < sayi ) {
+            System.out.println("Benim sayım: " + sayi);
+        }
+
+        System.out.println("-----------------------------");
+
+
+        int yas = 20;
+
+        if ( 18 <= yas ) {
+            System.out.println("Sağlıyor, Yaş durumu: " + yas);
+        }else {
+            System.out.println("Sağlamıyor, yaş: ");
+        }
+
+
+
+
+
+    }
+}
