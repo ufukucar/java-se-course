016f98cb7da5e8c1e77e0f4148622695478d163b
ufukucar <ufuk.ucar@tuseb.gov.tr>
2024-03-03 23:19:31 +0300

    switch case

---
+++ java-005-switch/src/main/java/com/ufukucar/App_Switch.java
@@ -0,0 +1,38 @@
+package com.ufukucar;
+
+public class App_Switch {
+    public static void main(String[] args) {
+
+
+        int secilen = 3;
+
+        switch (secilen) {
+
+            case 0:
+                System.out.println("Gidilecek menü: " + secilen);
+                break;
+
+            case 1:
+                System.out.println("Gidilecek menü: " + secilen);
+                break;
+            case 2:
+                System.out.println("Gidilecek menü: " + secilen);
+                break;
+            case 3:
+                System.out.println("Gidilecek menü: " + secilen);
+                break;
+            case 4:
+                System.out.println("Gidilecek menü: " + secilen);
+                break;
+
+            default:
+                System.out.println("Ana menüye dön");
+                break;
+        }
+
+
+
+
+
+    }
+}
