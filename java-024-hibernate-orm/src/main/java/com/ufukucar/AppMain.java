089416a02cae5c18311d6b6cc92435ea18c9667f
ufukucar <ufuk.ucar@tuseb.gov.tr>
2024-05-26 11:15:03 +0300

    HIBERNATE - ORM

--- java-024-hibernate-orm/src/main/java/com/ufukucar/AppMain.java
+++ java-024-hibernate-orm/src/main/java/com/ufukucar/AppMain.java
@@ -1,7 +1,18 @@
 package com.ufukucar;
 
-public class Main {
+public class AppMain {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
+
+
+
+
+
+
+
+
+
     }
 }
